10c506c5b58b2141e18830b2d6d0472a883e4030
Belal123-hub <131976726+Belal123-hub@users.noreply.github.com>
2024-08-02 03:04:12 +0700

    updated

---
+++ Bank-account-project/Diagrams/Enums/IncomeType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultilevelMenuExample.Enums
+{
+    public enum IncomeType
+    {
+        salary=1,
+        scholarship,
+        gift,
+        reward,
+        inheritance,
+        others
+    }
+}
